95848bf384ab4089498e2e44d9aa614acb1ac961
ghadirb <ghadir.baraty@gmail.com>
2025-09-09 09:57:28 +0330

    Delete generate_keystore.sh

--- generate_keystore.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-mkdir -p android/app/keystore
-keytool -genkey -v -keystore android/app/keystore/my-release-key.jks -alias my-key-alias -keyalg RSA -keysize 2048 -validity 10000
-echo "Keystore created at android/app/keystore/my-release-key.jks"
